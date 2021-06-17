1bd3ac8ed95694ac1b1e9f4fe6abc7067b50c375
Sa-lile <sachiyo.sable@gmail.com>
2021-06-17 23:05:07 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+// const operation = 10 * 10;
+// const operation = 10 % 5;
+
+// const operation = 7 % 5 ;
+// const operation = 2 ** 3;
+// 2 * 2 * 2
+// console.log(operation);
+
+// const str = " Juliette";
+// const str2 = "John";
+
+// console.log(`Je suis une chaine ${ str }, lorem lorem lorem ${ str2 }`);
+
+// function presenation(a,b){
+//     // return "Hello je suis Enzo";
+//     console.log("Hello je suis Enzo");
+//     return a + b;
+
+// }
+// // presenation();
+
+// console.log(presenation(4,6));
+
+
+// const foo = (a,b) => {
+//     console.log("Hello je suis Enzo");
+//     return a + b;
+// }
+// const foo = (a,b) => console.log("Hello je suis Enzo");
+
+// const btn = document.querySelector('#btn');
+
+// btn.addEventListener('click', () => {
+//     console.log("Tu as cliqué sur le bouton");
+// })
+// console.log(foo(4,6));
+
+function presentation() {
+
+    console.log(txt);
+}
+
+let txt = "Hello world"
+
+presentation();
+
+
+
+// const typeString =  "Enzo"; // String
+// const typeNumber = 9;
+// const typeUndefined = undefined;
+// const typeObj = {
+//     a : 1,
+//     b : 2,
+//     c : 3,
+// }
+// // console.log(typeObj.c);
+
+// const typeTableau = [1,2,3,4,5,6]
+// const typeBooleen = true;
+// // console.log(typeTableau[2]);
+
+// // prenom = "Hugo";
+
+// // let nom = "Bonaparte";
+// // nom = "Zola";
+
+
+// console.log(typeBooleen);
+
+
